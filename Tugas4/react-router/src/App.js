2c324dd1f7ed6a929d5cf8077c4afac4828269fc
hafizkalam <hafizkalamabdillah@google.com>
2022-03-23 11:39:11 +0700

    Tugas Praktikum

--- Tugas4/react-router/src/App.js
+++ Tugas4/react-router/src/App.js
@@ -107,85 +107,85 @@
 // }
 
 // LANGKAH PRAKTIKUM USE NESTING ROUTER
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useParams,
-  Outlet
-} from "react-router-dom";
+// import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link,
+//   useParams,
+//   Outlet
+// } from "react-router-dom";
 
-export default function NestingExample() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-        <hr/>
+// export default function NestingExample() {
+//   return (
+//     <Router>
+//       <div>
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/topics">Topics</Link>
+//           </li>
+//         </ul>
+//         <hr/>
 
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/topics" element={<Topics />}/>
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+//         <Routes>
+//           <Route path="/" element={<Home />}/>
+//           <Route path="/topics" element={<Topics />}/>
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
 
-function Home() {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-}
+// function Home() {
+//   return (
+//     <div>
+//       <h2>Home</h2>
+//     </div>
+//   );
+// }
 
-function Topics() {
-  // path untuk membuat jalur <Route> yang terhadap rute induk,
-  // url digunakan untuk membuat link.
-  let { path, url } = Outlet();
-  return (
-    <div>
-      <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${url}/Sate, Nasi Goreng`}>Kuliner</Link>
-      </li>
-      <li>
-        <Link to={`${url}/Wisata Alam, Nasi Goreng`}>Travelling</Link>
-      </li>
-      <li>
-        <Link to={`${url}/Ibis, JW Merriot`}>Review Hotel</Link>
-      </li>
-    </ul>
-    <hr/>
-    <Routes>
-      <Route path={path}>
-        <h3>Please select a topic</h3>
-      </Route>
-      <Route path={`${path}/:topicId`} element={<Topic />}/>
-    </Routes>
-    </div>
-  );
-}
+// function Topics() {
+//   // path untuk membuat jalur <Route> yang terhadap rute induk,
+//   // url digunakan untuk membuat link.
+//   let { path, url } = Outlet();
+//   return (
+//     <div>
+//       <h2>Topics</h2>
+//     <ul>
+//       <li>
+//         <Link to={`${url}/Sate, Nasi Goreng`}>Kuliner</Link>
+//       </li>
+//       <li>
+//         <Link to={`${url}/Wisata Alam, Nasi Goreng`}>Travelling</Link>
+//       </li>
+//       <li>
+//         <Link to={`${url}/Ibis, JW Merriot`}>Review Hotel</Link>
+//       </li>
+//     </ul>
+//     <hr/>
+//     <Routes>
+//       <Route path={path}>
+//         <h3>Please select a topic</h3>
+//       </Route>
+//       <Route path={`${path}/:topicId`} element={<Topic />}/>
+//     </Routes>
+//     </div>
+//   );
+// }
 
-function Topic() {
-  let { topicId } = useParams();
+// function Topic() {
+//   let { topicId } = useParams();
 
-  return (
-    <div>
-      <h3>{topicId}</h3>
-    </div>
-  );   
-}
+//   return (
+//     <div>
+//       <h3>{topicId}</h3>
+//     </div>
+//   );   
+// }
 
 // LANGKAH PRAKTIKUM USE REDIRECTS (AUTH)
 // import React, { useContext, createContext, useState } from "react";
@@ -335,3 +335,169 @@ function Topic() {
 //     </div>
 //   );
 // }
+
+// Tugas Praktikum
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+  useNavigate,
+  Navigate
+} from "react-router-dom";
+
+export default function AuthExample() {
+  return (
+    <Router>
+      <div>
+        <AuthButton />
+
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/private">Private</Link>
+          </li>
+        </ul>
+        <hr />
+        <Routes>
+          <Route path="/" element={<PublicPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route 
+            path="/private/*" 
+            element={
+              <PrivateRoute >
+                <Barangs />
+              </PrivateRoute>
+            } 
+          ></Route>
+          <Route 
+            path="/private/barang/Elektronik/*" 
+            element={
+              <PrivateRoute >
+                <Barangs >
+                  <BarangElektroniks />
+                </Barangs>
+              </PrivateRoute>
+            } 
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+const fakeAuth = {
+  isAuthenticated : false,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100);
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+function AuthButton() {
+  let history = useNavigate();
+
+  return fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome!{" "}
+      <button
+        onClick={() => {
+          fakeAuth.signout(() => history.push("/"));
+        }}
+        >
+          Sign out
+        </button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  );
+}
+
+function PrivateRoute({ children }) {
+  return fakeAuth.isAuthenticated ? ( children ) : <Navigate to={{ pathname: "/login" }} />;
+}
+
+function PublicPage() {
+  return <h3>Public</h3>;
+}
+
+function ProtectedPage() {
+  return <h3>Private</h3>
+}
+
+function LoginPage() {
+  let history = useNavigate();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You must log in to view the page at { from.pathname }</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
+}
+
+// NESTED
+function Barangs() {
+  return (
+    <div>
+      <h2>Barang</h2>
+      <ul>
+        <li>
+          <Link to={"/private/barang/Elektronik"}>Elektronik</Link>
+        </li>
+        <li>
+          <Link to={"/private/barang/Pakaian"}>Pakaian</Link>
+        </li>
+        <li>
+          <Link to={"/private/barang/Sepatu"}>Sepatu</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        {/* <Route path="/" element={<h3>Harap pilih barang.</h3>} /> */}
+        <Route path="/private/barang/Elektronik" element={<BarangElektroniks />} />
+        <Route path="/private/barang/Pakaian" element={<BarangPakaian />} />
+        <Route path="/private/barang/Sepatu" element={<BarangSepatu />} />
+      </Routes>
+    </div>
+  );
+}
+
+function BarangElektroniks() {
+  return (
+    <div>
+      <h2>Barang Elektronik</h2>
+    </div>
+  );
+}
+
+function BarangPakaian() {
+  return (
+    <div>
+      <h2>Barang Pakaian</h2>
+    </div>
+  );
+}
+function BarangSepatu() {
+  return (
+    <div>
+      <h2>Barang Sepatu</h2>
+    </div>
+  );
+}
